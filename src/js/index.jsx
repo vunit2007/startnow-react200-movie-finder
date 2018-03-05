953ea2474980de0cdbff9de0a3dd1b909d43dcee
Vy Nguyen <vy.t.nguyen.11@gmail.com>
2018-03-04 23:28:06 -0800

    install/configure redux in js/index.jsx

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
 import reducers from './reducers';
 import App from './app';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducers, composeEnhancers(applyMiddleware(promiseMiddleware())));
 /* eslint-enable */
-
 
 render(
   <Provider store={ store }>
